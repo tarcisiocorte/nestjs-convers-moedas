b00803b29a37322eaaae228675a6a4005dbb113b
tarcisiocorte <tarcisiocorte@gmail.com>
2021-05-26 00:11:47 -0300

    ensure converAmount return correct converted value

--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+
+export class CurrenciesService {
+  async getCurrency(currency: string): Promise<any> {}
+}
 
 @Injectable()
 export class ExchangeService {
-    async convertAmount({from, to, amount}): Promise<any> {
-        throw new Error();
+  constructor(private currenciesService: CurrenciesService) {}
+
+  async convertAmount({ from, to, amount }): Promise<any> {
+    if (!from || !to || !amount) {
+      throw new BadRequestException();
     }
+
+    try {
+      const currencyFrom = await this.currenciesService.getCurrency(from);
+      const currencyTo = await this.currenciesService.getCurrency(to);
+
+      return { amount: (currencyFrom.value / currencyTo.value) * amount };
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 }
